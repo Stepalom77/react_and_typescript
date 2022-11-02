999f37eee2183d69a5023555aeb6eb19a657e4dd
Stepalom77 <stepalom22@gmail.com>
2022-11-02 16:53:08 -0500

    refs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './index.css';
-import EventComponent from './events/EventComponent';
+import UserSearch from './refs/UserSearch';
 
 function App() {
   return (
     <div className="App">
-      <EventComponent/>
+      <UserSearch/>
     </div>
   );
 }
